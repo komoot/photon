395e6d9b6329a09369d50cea50631bcd7db832b4
Christoph Lingg <christoph@lingg.eu>
2014-02-20 12:49:18 +0100

    convert xml for testing purposes

---
+++ convert_xml.py
@@ -0,0 +1,78 @@
+# script to convert solr xml to check out other approaches
+
+import xml.etree.cElementTree as ET
+
+input_xml = 'sample_data/iceland.solr.xml'
+iter = ET.iterparse(input_xml)
+
+output_xml = input_xml.replace('.xml', '.extended.xml')
+
+
+def get_search_field(doc, lang, housenumber_first=False):
+    field = []
+    field.append(doc.get('name_' + lang))
+    field.append(doc.get('name'))
+
+    if housenumber_first:
+        field.append(doc.get('street'))
+        field.append(doc.get('housenumber'))
+    else:
+        field.append(doc.get('street'))
+        field.append(doc.get('housenumber'))
+
+    field.append(doc.get('postcode'))
+    field.append(doc.get('city_' + lang))
+    field.append(doc.get('city'))
+    field.append(doc.get('country_' + lang))
+    field.append(doc.get('country'))
+
+    places_local = doc.get('places_' + lang)
+    if places_local:
+        places_local = [t.strip() for t in places_local.split(',')]
+
+    places = doc.get('places')
+    if places:
+        places = [t.strip() for t in places.split(',')]
+
+    unified_places = set()
+
+    if places_local:
+        for p in places_local:
+            unified_places.add(p)
+
+    if places:
+        for p in places:
+            unified_places.add(p)
+
+    if len(unified_places):
+        field += list(places)
+
+    return "; ".join(filter(None, field))
+
+
+count = 0
+with open(output_xml, 'w') as f:
+    f.write("<?xml version='1.0' encoding='utf-8'?>\n<add>\n")
+
+    doc = {}
+
+    for event, elem in iter:
+        count += 1
+
+        if not count % 10000:
+            print("progress: {:10d}".format(count))
+
+        if elem.tag == 'doc':
+            doc['search_de'] = get_search_field(doc, 'de', housenumber_first=True)
+            doc['search_en'] = get_search_field(doc, 'en')
+            doc['search_fr'] = get_search_field(doc, 'fr')
+            doc['search_it'] = get_search_field(doc, 'it', housenumber_first=True)
+
+            f.write('\t<doc>\n{}\t</doc>\n'.format(
+                "".join(["""\t\t<field name="{0}">{1}</field>\n""".format(*field) for field in doc.items()]))
+            )
+            doc = {}
+        elif 'name' in elem.attrib:
+            doc[elem.attrib['name']] = elem.text
+
+    f.write("</add>")
