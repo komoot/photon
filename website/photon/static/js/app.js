45f80dff720884fe110712cd8b132bfe829679ba
Yohan Boniface <yb@enix.org>
2014-02-20 17:29:06 +0100

    Display markers when searching

--- website/photon/static/js/app.js
+++ website/photon/static/js/app.js
@@ -1,3 +1,14 @@
-var startPoint = [52.3879, 13.0582];
-var map = L.map('map', {scrollWheelZoom: false, photonControl: true}).setView(startPoint, 12),
-    tilelayer = L.tileLayer('http://{s}.tile.komoot.de/komoot/{z}/{x}/{y}.png', {maxZoom: 20, attribution: 'Data \u00a9 <a href="http://www.openstreetmap.org/copyright"> OpenStreetMap Contributors </a> Tiles \u00a9 HOT'}).addTo(map);
+var startPoint = [52.3879, 13.0582],
+    searchPoints = L.geoJson(null, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
+function showSearchPoints (geojson) {
+    searchPoints.clearLayers();
+    searchPoints.addData(geojson);
+}
+var map = L.map('map', {scrollWheelZoom: false, photonControl: true, photonControlOptions: {resultsHandler: showSearchPoints}});
+map.setView(startPoint, 12);
+searchPoints.addTo(map);
+var tilelayer = L.tileLayer('http://{s}.tile.komoot.de/komoot/{z}/{x}/{y}.png', {maxZoom: 20, attribution: 'Data \u00a9 <a href="http://www.openstreetmap.org/copyright"> OpenStreetMap Contributors </a> Tiles \u00a9 Komoot'}).addTo(map);
