64b9280b6ebe2301b70a649f2d9e2c293762222c
Yohan Boniface <yb@enix.org>
2014-02-20 19:37:03 +0100

    Photon tiles go to zoom 18, not 20

--- website/photon/static/js/app.js
+++ website/photon/static/js/app.js
@@ -11,5 +11,5 @@ function showSearchPoints (geojson) {
 var map = L.map('map', {scrollWheelZoom: false, zoomControl: false, photonControl: true, photonControlOptions: {resultsHandler: showSearchPoints, placeholder: 'Try me…', position: 'topleft'}});
 map.setView(startPoint, 12);
 searchPoints.addTo(map);
-var tilelayer = L.tileLayer('http://{s}.tile.komoot.de/komoot/{z}/{x}/{y}.png', {maxZoom: 20, attribution: 'Data \u00a9 <a href="http://www.openstreetmap.org/copyright"> OpenStreetMap Contributors </a> Tiles \u00a9 Komoot'}).addTo(map);
+var tilelayer = L.tileLayer('http://{s}.tile.komoot.de/komoot/{z}/{x}/{y}.png', {maxZoom: 18, attribution: 'Data \u00a9 <a href="http://www.openstreetmap.org/copyright"> OpenStreetMap Contributors </a> Tiles \u00a9 Komoot'}).addTo(map);
 var zoomControl = new L.Control.Zoom({position: 'topright'}).addTo(map);
