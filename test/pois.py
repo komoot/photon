fb8ab43f808cb82dc51784505dd424da2773e62f
Yohan Boniface <yb@enix.org>
2014-02-21 15:30:25 +0100

    Mode POIs tests

--- test/pois.py
+++ test/pois.py
@@ -4,8 +4,15 @@ from test.base import PhotonImplementationTest
 
 
 class Pois(PhotonImplementationTest):
+
     def test_teufelsberg(self):
         self.assertMatch("teufelsberg ber", {'osm_id': 156850034})
+
+    def test_c_tout_bio_cergy(self):
+        self.assertMatch("c tout bio cergy", {'osm_id': 2406109852}, comment="I can find 'C'Tout Bio' shop in Cergy")
+
+    def test_canal_bio_paris(self):
+        self.assertMatch("canal bio paris", {'osm_id': 627857438}, comment="I can find 'Canal Bio' shop in Paris")
 
 
 if __name__ == '__main__':
