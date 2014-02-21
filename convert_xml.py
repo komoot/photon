b47faaf1f4204554add521a10f08966159113dce
Christoph Lingg <christoph@lingg.eu>
2014-02-21 20:40:16 +0100

    check converter on server

--- convert_xml.py
+++ convert_xml.py
@@ -1,89 +1,74 @@
 # script to convert solr xml to check out other approaches
+import json
 
 import xml.etree.cElementTree as et
-from xml.sax.saxutils import escape
+
+#input_xml = '/home/photon/data/solr-131012.xml'
+input_xml = 'sample_data/iceland.solr.xml'
+
+I18N_FIELDS = "name", "city", "country", "places"
+int_fields = []
+int_fields += I18N_FIELDS
+
+LANGUAGES = ["de", "en", "fr", "it", 'es']
+for field in I18N_FIELDS:
+    int_fields += [field + "_" + lang for lang in LANGUAGES]
 
 
-def get_search_field(doc, lang, housenumber_first=False):
-    field = []
-    field.append(doc.get('name_' + lang))
-    field.append(doc.get('name'))
+def nested_keys(field_name):
+    keys = field_name.split("_")
 
-    if housenumber_first:
-        field.append(doc.get('street'))
-        field.append(doc.get('housenumber'))
-    else:
-        field.append(doc.get('street'))
-        field.append(doc.get('housenumber'))
+    if len(keys) < 2:
+        keys.append('default')
 
-    field.append(doc.get('postcode'))
-    field.append(doc.get('city_' + lang))
-    field.append(doc.get('city'))
-    field.append(doc.get('country_' + lang))
-    field.append(doc.get('country'))
+    if keys[0] == 'places':
+        keys[0] = 'context'
 
-    places_local = doc.get('places_' + lang)
-    if places_local:
-        places_local = [t.strip() for t in places_local.split(',')]
-
-    places = doc.get('places')
-    if places:
-        places = [t.strip() for t in places.split(',')]
-
-    unified_places = set()
-
-    if places_local:
-        for p in places_local:
-            unified_places.add(p)
-
-    if places:
-        for p in places:
-            unified_places.add(p)
-
-    if len(unified_places):
-        field += list(unified_places)
-
-    return "; ".join(filter(None, field))
+    return keys
 
 
-input_xml = '/home/photon/data/solr-131012.xml'
-#input_xml = 'sample_data/iceland.solr.xml'
-xml_iter = et.iterparse(input_xml)
+ONLY_GERMANY = True
 
-output_xml = input_xml.replace('.xml', '.extended.xml')
+INSERT_LINE = """{ "index" : { "_index" : "photon", "_type" : "place"} }\n"""
+
+xml_tree = et.iterparse(input_xml)
+output_xml = input_xml.replace('.xml', '.json')
 
 count = 0
 with open(output_xml, 'w') as f:
-    f.write("<?xml version='1.0' encoding='utf-8'?>\n<add>\n")
-
     doc = {}
 
-    for event, elem in xml_iter:
-        count += 1
+    for event, elem in xml_tree:
+        if elem.tag == 'field':
+            attr = elem.attrib['name']
 
-        if not count % 100000:
-            print("progress: {:,}".format(count))
+            if attr in int_fields:
+                key, sub_key = nested_keys(attr)
+                if key not in doc:
+                    doc[key] = {}
 
-        if elem.tag == 'doc':
-            doc['search_de'] = get_search_field(doc, 'de', housenumber_first=True)
-            doc['search_en'] = get_search_field(doc, 'en')
-            doc['search_fr'] = get_search_field(doc, 'fr')
-            doc['search_it'] = get_search_field(doc, 'it', housenumber_first=True)
-
-            f.write("\t<doc>\n")
-
-            for field in doc.items():
-                if field[0] is None or field[1] is None:
+                if sub_key == "es":
                     continue
 
-                f.write("""\t\t<field name="{0}">{1}</field>\n""".format(field[0], escape(field[1])))
+                doc[key][sub_key] = elem.text
+            elif attr == "ranking":
+                doc[attr] = int(elem.text)
+            else:
+                doc[attr] = elem.text
 
-            f.write("\t</doc>\n")
+        elif elem.tag == 'doc':
+            # document is finished, dump it to json
+            if not ONLY_GERMANY or doc.get('country', {}).get('default') == "Deutschland":
+                count += 1
+                if not count % 1000:
+                    print("progress: {:,}".format(count))
+
+                f.write(INSERT_LINE)
+                json.dump(doc, f)
+                f.write("\n")
 
             doc = {}
-        elif 'name' in elem.attrib:
-            doc[elem.attrib['name']] = elem.text
 
         elem.clear()
-        
-    f.write("</add>")
+
+    f.write("\n")
