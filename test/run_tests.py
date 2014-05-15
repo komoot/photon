c50df8bdbd780da00181d0fd7ae6976a21281b32
Benoit Richter <benoit.richter@opendatasoft.com>
2014-05-15 17:44:23 +0200

    Hacky test runner

--- test/run_tests.py
+++ test/run_tests.py
@@ -2,24 +2,52 @@ import unittest
 import sys
 import os
 import csv
+import requests
 
-from base import PhotonImplementationTest
+from dataset import Dataset
 
-queries_folder = sys.argv[1] if len(sys.argv) > 1 else 'queries'
+queries_folder = 'iledefrance'
 queries_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), queries_folder)
 
-class CSVSourceTests(PhotonImplementationTest):
+URL = "http://photon.komoot.de/api/"
 
-    def test_from_csv(self):
-        for filepath in os.listdir(queries_folder_path):
-            filename, filext = os.path.splitext(filepath)
-            if os.path.isfile(os.path.join(queries_folder, filepath)) and filext == '.csv':
-                # This is a queries file
-                print('* Running test suite from file %s' % filename)
-                with open(os.path.join(queries_folder_path, filepath)) as csv_file:
-                    reader = csv.DictReader(csv_file)
-                    for query in reader:
-                        print(query)
+class CSVDatasetTests(unittest.TestCase):
+    def assertMatch(self, search, expected, limit=1, comment=None, lang=None):
+        params = {"q": search, "limit": limit}
+        if lang:
+            params['lang'] = lang
+        results = requests.get(URL, params=params).json()
+        
+        data = results['features'][0]['properties']
+
+        if expected.get('expected_city'):
+            self.assertTrue('city' in data, 'There is no city property in the result')
+            self.assertEqual(data['city'], expected['expected_city'], 'Returned city is not the expected city')
+        if expected.get('expected_country'):
+            self.assertEqual(data['country'], expected['expected_country'], 'Returned country is not the expected country')
+        if expected.get('expected_street'):
+            self.assertEqual(data['street'], expected['expected_street'], 'Returned street is not the expected street')
+        if expected.get('expected_postcode'):
+            self.assertEqual(data['postcode'], expected['expected_postcode'], 'Returned postcode is not the expected postcode')
+            
+        def assertExpected(expected):
+            found = False
+            for r in results['features']:
+                found = True
+                for key, value in expected.items():
+                    if not key in r['properties'] or not r['properties'][key] == value:
+                        found = False
+            if not found:
+                if comment:
+                    msg = "{}\nResults were:\n{}".format(comment, results)
+                else:
+                    msg = "{expected} not found in {results}".format(results=results, expected=expected)
+                self.fail(msg)
+
+    def tests_from_datasets(self):
+        for testset in Dataset.import_from_path(queries_folder_path):
+            print('Testing query: %s' % testset['tried_query'])
+            self.assertMatch(testset['tried_query'], testset)
 
 
 if __name__ == '__main__':
