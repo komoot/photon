6adf5a622175d4de55235355039440a91ad84c24
Christoph Lingg <christoph@lingg.eu>
2014-05-18 17:28:11 +0200

    automatic install missing of custom word ending plugin

--- src/main/java/de/komoot/photon/importer/elasticsearch/Server.java
+++ src/main/java/de/komoot/photon/importer/elasticsearch/Server.java
@@ -9,9 +9,14 @@ import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.env.Environment;
 import org.elasticsearch.node.Node;
+import org.elasticsearch.plugins.PluginManager;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -36,11 +41,19 @@ public class Server {
 	 * starts the elasticsearch node
 	 */
 	public void start() {
-		ImmutableSettings.Builder settings =
-				ImmutableSettings.settingsBuilder();
-		settings.put("path.home", this.esDirectory.toString());
+		Settings settings = ImmutableSettings.settingsBuilder()
+				.put("path.home", this.esDirectory.toString())
+				.build();
 
+		final Environment environment = new Environment(settings);
+		PluginManager pluginManager = new PluginManager(environment, "http://photon.komoot.de/data/elasticsearch-wordending-tokenfilter-0.0.1.zip", PluginManager.OutputMode.VERBOSE, new TimeValue(30000));
+		try {
+			pluginManager.downloadAndExtract("ybon/elasticsearch-wordending-tokenfilter/0.0.1");
+		} catch(IOException e) {
+			log.debug("could not install wordending-tokenfilter", e);
+		}
 		this.esNode = nodeBuilder().clusterName(this.clusterName).loadConfigSettings(true).settings(settings).node();
+		log.info("started elastic search node");
 	}
 
 	/**
@@ -106,7 +119,7 @@ public class Server {
 		this.dumpDirectory = new File(photonDirectory, "dumps");
 		this.updateDirectory = new File(photonDirectory, "updates");
 
-		for(File directory : new File[]{esDirectory, dumpDirectory, updateDirectory, photonDirectory}) {
+		for(File directory : new File[]{esDirectory, dumpDirectory, updateDirectory, photonDirectory, new File(photonDirectory, "elasticsearch/plugins")}) {
 			if(!directory.exists())
 				directory.mkdirs();
 		}
