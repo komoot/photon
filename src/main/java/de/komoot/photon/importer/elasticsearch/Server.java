328b8f787b2d3a7aca0c0c0076763554526392f1
Christoph Lingg <christoph@lingg.eu>
2014-05-19 01:50:31 +0200

    formatting

--- src/main/java/de/komoot/photon/importer/elasticsearch/Server.java
+++ src/main/java/de/komoot/photon/importer/elasticsearch/Server.java
@@ -16,6 +16,7 @@ import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.plugins.PluginManager;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -32,170 +33,153 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 @Slf4j
 public class Server {
 
-    private Node esNode;
-    private String clusterName;
-    private File mainDirectory;
-    private File esDirectory;
-    private File dumpDirectory;
-    private File updateDirectory;
-    private File tempDirectory;
-    private File importDirectory;
+	private Node esNode;
+	private String clusterName;
+	private File mainDirectory;
+	private File esDirectory;
+	private File dumpDirectory;
+	private File updateDirectory;
+	private File tempDirectory;
+	private File importDirectory;
 
-    public void start() {
-        Settings settings = ImmutableSettings.settingsBuilder()
-                .put("path.home", this.esDirectory.toString())
-                .build();
+	public void start() {
+		Settings settings = ImmutableSettings.settingsBuilder()
+				.put("path.home", this.esDirectory.toString())
+				.build();
 
-        final String pluginPath = this.getClass().getResource("/elasticsearch-wordending-tokenfilter-0.0.1.zip").toExternalForm();
-        PluginManager pluginManager = new PluginManager(new Environment(settings), pluginPath, PluginManager.OutputMode.VERBOSE, new TimeValue(30000));
-        try {
-            pluginManager.downloadAndExtract("ybon/elasticsearch-wordending-tokenfilter/0.0.1");
-        } catch(IOException e) {
-            log.debug("could not install ybon/elasticsearch-wordending-tokenfilter/0.0.1", e);
-        }
+		final String pluginPath = this.getClass().getResource("/elasticsearch-wordending-tokenfilter-0.0.1.zip").toExternalForm();
+		PluginManager pluginManager = new PluginManager(new Environment(settings), pluginPath, PluginManager.OutputMode.VERBOSE, new TimeValue(30000));
+		try {
+			pluginManager.downloadAndExtract("ybon/elasticsearch-wordending-tokenfilter/0.0.1");
+		} catch(IOException e) {
+			log.debug("could not install ybon/elasticsearch-wordending-tokenfilter/0.0.1", e);
+		}
 
-        pluginManager = new PluginManager(new Environment(settings), null, PluginManager.OutputMode.VERBOSE, new TimeValue(30000));
-        for(String pluginName : new String[]{"mobz/elasticsearch-head", "polyfractal/elasticsearch-inquisitor", "elasticsearch/marvel/latest"}) {
-            try {
-                pluginManager.downloadAndExtract(pluginName);
-            } catch(IOException e) {
-            }
-        }
+		pluginManager = new PluginManager(new Environment(settings), null, PluginManager.OutputMode.VERBOSE, new TimeValue(30000));
+		for(String pluginName : new String[]{"mobz/elasticsearch-head", "polyfractal/elasticsearch-inquisitor", "elasticsearch/marvel/latest"}) {
+			try {
+				pluginManager.downloadAndExtract(pluginName);
+			} catch(IOException e) {
+			}
+		}
 
-        this.esNode = nodeBuilder().clusterName(this.clusterName).loadConfigSettings(true).settings(settings).node();
-        log.info("started elastic search node");
-    }
+		this.esNode = nodeBuilder().clusterName(this.clusterName).loadConfigSettings(true).settings(settings).node();
+		log.info("started elastic search node");
+	}
 
+	/**
+	 * stops the elasticsearch node
+	 */
+	public void shutdown() {
+		this.esNode.close();
+	}
 
+	public void createSnapshot(String dumpName) {
+		log.info(String.format("Create snapshot '%s' in %s", dumpName, this.dumpDirectory.getAbsolutePath()));
 
-    /**
-     * stops the elasticsearch node
-     */
-    public void shutdown(){
-        this.esNode.close();
-    }
+		final File dumpLocation = new File(this.dumpDirectory, dumpName);
+		dumpLocation.mkdir();
+		this.esNode.client().admin().cluster().putRepository(this.getClient().admin().cluster().preparePutRepository(dumpName)
+				.setSettings(ImmutableSettings.settingsBuilder().put("compress", "true")
+						.put("location", dumpLocation.getAbsolutePath())).setType("fs").request()).actionGet();
 
+		this.getClient().admin().cluster().createSnapshot(this.getClient().admin().cluster().
+				prepareCreateSnapshot(dumpName, dumpName).setIndices("photon").request(), new ActionListener<CreateSnapshotResponse>() {
+			@Override
+			public void onResponse(CreateSnapshotResponse createSnapshotResponse) {
+				try {
+					ZipFile zipFile = new ZipFile(dumpLocation + ".zip");
+					ZipParameters parameters = new ZipParameters();
+					parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+					parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+					zipFile.addFolder(dumpLocation, parameters);
+					dumpLocation.delete();
 
-    public void createSnapshot(String dumpName) {
-        log.info(String.format("Create snapshot '%s' in %s", dumpName, this.dumpDirectory.getAbsolutePath()));
+					log.info(String.format("Created snapshot: %s.zip", dumpLocation));
+				} catch(ZipException e) {
+					log.error("error creating zip file of snapshot", e);
+				}
+			}
 
-        final File dumpLocation = new File(this.dumpDirectory, dumpName);
-        dumpLocation.mkdir();
-        this.esNode.client().admin().cluster().putRepository(this.getClient().admin().cluster().preparePutRepository(dumpName)
-                .setSettings(ImmutableSettings.settingsBuilder().put("compress", "true")
-                        .put("location", dumpLocation.getAbsolutePath())).setType("fs").request()).actionGet();
+			@Override
+			public void onFailure(Throwable e) {
+				log.error("error creating snapshot", e);
+			}
+		});
+	}
 
-        this.getClient().admin().cluster().createSnapshot(this.getClient().admin().cluster().
-                prepareCreateSnapshot(dumpName, dumpName).setIndices("photon").request(), new ActionListener<CreateSnapshotResponse>() {
-            @Override
-            public void onResponse(CreateSnapshotResponse createSnapshotResponse) {
-                try {
-                    ZipFile zipFile = new ZipFile(dumpLocation + ".zip");
-                    ZipParameters parameters = new ZipParameters();
-                    parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
-                    parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
-                    zipFile.addFolder(dumpLocation, parameters);
-                    dumpLocation.delete();
+	public void importSnapshot(String dumpUrl, String dumpName) {
+		File dump = new File(this.tempDirectory, dumpName + ".zip");
+		String dumpLocation = "";
+		try {
+			FileUtils.copyFile(new File(new URL(dumpUrl).toURI()), dump);
+		} catch(Exception e) {
+			log.error("Error while loading dump (Is this the correct dump location?)", e);
+		}
 
-                    log.info(String.format("Created snapshot: %s.zip", dumpLocation));
-                } catch(ZipException e) {
-                    log.error("error creating zip file of snapshot", e);
-                }
-            }
+		try {
+			ZipFile dumpZip = new ZipFile(dump);
+			dumpLocation = this.importDirectory.getAbsolutePath() + dumpName;
+			dumpZip.extractAll(dumpLocation);
+		} catch(ZipException e) {
 
-            @Override
-            public void onFailure(Throwable e) {
-                log.error("error creating snapshot", e);
-            }
-        });
-    }
+			log.error("Can´t unzip dump", e);
+		}
+		this.getClient().admin().cluster().getSnapshots(this.getClient().admin().cluster().prepareGetSnapshots(dumpLocation).setSnapshots(dumpName).request(), new ActionListener<GetSnapshotsResponse>() {
+			@Override
+			public void onResponse(GetSnapshotsResponse getSnapshotsResponse) {
+				log.info("Import done!");
+			}
 
+			@Override
+			public void onFailure(Throwable e) {
+				log.error("error creating snapshot", e);
+			}
+		});
+	}
 
+	/**
+	 * returns an elasticsearch client
+	 */
+	public Client getClient() {
+		return this.esNode.client();
+	}
 
-    public void importSnapshot(String dumpUrl, String dumpName){
-        File dump = new File(this.tempDirectory, dumpName+".zip");
-        String dumpLocation = "";
-        try{
-            FileUtils.copyFile(new File(new URL(dumpUrl).toURI()), dump );
-        } catch (Exception e){
-            log.error("Error while loading dump (Is this the correct dump location?)", e);
-        }
+	private File setupDirectories(URL directoryName) {
 
-        try{
-            ZipFile dumpZip = new ZipFile(dump);
-            dumpLocation =    this.importDirectory.getAbsolutePath()+dumpName;
-            dumpZip.extractAll(dumpLocation);
+		File mainDirectory = new File(".");
 
+		try {
+			mainDirectory = new File(directoryName.toURI());
+		} catch(URISyntaxException e) {
+			log.error("Can´t access photon_data directory");
+		}
 
-        } catch (ZipException e){
+		File photonDirectory = new File(mainDirectory, "photon_data");
+		this.esDirectory = new File(photonDirectory, "elasticsearch");
+		this.dumpDirectory = new File(photonDirectory, "dumps");
+		this.updateDirectory = new File(photonDirectory, "updates");
+		this.tempDirectory = new File(photonDirectory, "temp");
+		this.importDirectory = new File(photonDirectory, "imports");
 
-            log.error("Can´t unzip dump", e);
+		for(File directory : new File[]{esDirectory, dumpDirectory, updateDirectory, importDirectory, tempDirectory, photonDirectory, new File(photonDirectory, "elasticsearch/plugins")}) {
+			if(!directory.exists())
+				directory.mkdirs();
+		}
 
-        }
-        this.getClient().admin().cluster().getSnapshots(this.getClient().admin().cluster().prepareGetSnapshots(dumpLocation).setSnapshots(dumpName).request(),new ActionListener<GetSnapshotsResponse>() {
-            @Override
-            public void onResponse(GetSnapshotsResponse getSnapshotsResponse) {
-                log.info("Import done!");
-            }
+		return mainDirectory;
+	}
 
-            @Override
-            public void onFailure(Throwable e) {
-                log.error("error creating snapshot", e);
-            }
-        });
+	/**
+	 * @param clusterName name of the elasticsearch cluster
+	 */
+	public Server(String clusterName, String mainDirectory) {
 
-
-
-
-    }
-
-
-    /**
-     * returns an elasticsearch client
-     */
-    public Client getClient(){
-        return this.esNode.client();
-    }
-
-
-    private File setupDirectories(URL directoryName) {
-
-        File mainDirectory = new File(".");
-
-        try {
-            mainDirectory = new File(directoryName.toURI());
-        } catch(URISyntaxException e) {
-            log.error("Can´t access photon_data directory");
-        }
-
-        File photonDirectory = new File(mainDirectory, "photon_data");
-        this.esDirectory = new File(photonDirectory, "elasticsearch");
-        this.dumpDirectory = new File(photonDirectory, "dumps");
-        this.updateDirectory = new File(photonDirectory, "updates");
-        this.tempDirectory = new File(photonDirectory, "temp");
-        this.importDirectory = new File(photonDirectory, "imports");
-
-
-
-        for(File directory : new File[]{esDirectory, dumpDirectory, updateDirectory, importDirectory, tempDirectory, photonDirectory, new File(photonDirectory, "elasticsearch/plugins")}) {
-            if(!directory.exists())
-                directory.mkdirs();
-        }
-
-        return mainDirectory;
-    }
-
-    /**
-     * @param clusterName name of the elasticsearch cluster
-     */
-    public Server(String clusterName, String mainDirectory){
-
-        try{
-        this.mainDirectory = setupDirectories(new URL("file://"+mainDirectory));
-        } catch (MalformedURLException e)
-        {
-            log.error("Can´t create directories");
-        }
-        this.clusterName = "defdeweef";
-    }
+		try {
+			this.mainDirectory = setupDirectories(new URL("file://" + mainDirectory));
+		} catch(MalformedURLException e) {
+			log.error("Can´t create directories");
+		}
+		this.clusterName = "defdeweef";
+	}
 }
