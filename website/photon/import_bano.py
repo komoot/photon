32ec83498bc3cf460fed8ff7cfb6f115a5f02338
Yohan Boniface <yb@enix.org>
2014-06-28 12:26:07 +0200

    BANO: dump to file

--- website/photon/import_bano.py
+++ website/photon/import_bano.py
@@ -3,6 +3,7 @@
 import csv
 import sys
 import os
+import json
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk_index
 
@@ -10,43 +11,7 @@ ES = Elasticsearch()
 INDEX = 'photon'
 DOC_TYPE = 'place'
 FILEPATH = os.environ.get('BANO_FILEPATH', 'bano.csv')
-
-
-"""
-{
-  "osm_id": 2525485803,
-  "osm_type": "N",
-  "osm_key": "place",
-  "osm_value": "house",
-  "importance": 0.0,
-  "coordinate": {
-    "lat": 48.6351388,
-    "lon": 2.4480519
-  },
-  "housenumber": "3",
-  "postcode": "91000",
-  "city": {
-    "default": "Évry",
-    "fr": "Évry"
-  },
-  "country": {
-    "de": "Frankreich",
-    "it": "Francia",
-    "default": "France",
-    "fr": "France",
-    "en": "France"
-  },
-  "street": {
-    "default": "Rue du Château"
-  },
-  "context": {
-    "it": "Isola di Francia",
-    "default": "Île-de-France, Résidence du Petit Bois, Essonne, Évry",
-    "fr": "Île-de-France, Essonne",
-    "en": "Ile-de-France"
-  }
-}
-"""
+DUMPPATH = os.environ.get('BANO_DUMPPATH', '/tmp')
 
 fields = [
     'source_id', 'housenumber', 'street', 'postcode', 'city', 'source', 'lat',
@@ -121,6 +86,23 @@ def cleanup():
         body=query
     )
 
+
+def index(data):
+    print('Start indexing batch of', len(data))
+    bulk_index(ES, data, index=INDEX, doc_type=DOC_TYPE, refresh=True)
+    print('End indexing of current batch')
+
+
+def dump(data, idx):
+    path = os.path.join(
+        DUMPPATH,
+        'bano_dump_{}'.format(idx)
+    )
+    with open(path, mode='w', encoding='utf-8') as f:
+        f.write('\n'.join(data))
+        sys.stdout.write('Dump {0}\n'.format(path))
+
+
 if __name__ == "__main__":
     # first cleanup the housenumber data (we don't want duplicates)
     cleanup()
@@ -128,12 +110,13 @@ if __name__ == "__main__":
         reader = csv.DictReader(f, fieldnames=fields)
         count = 0
         data = []
+        idx = 0
         for row in reader:
-            data.append(row_to_doc(row))
+            data.append('{"index": {}}')
+            data.append(json.dumps(row_to_doc(row)))
             count += 1
             if count % 100000 == 0:
-                print('Start indexing batch of', len(data))
-                bulk_index(ES, data, index=INDEX, doc_type=DOC_TYPE, refresh=True)
-                print('End indexing of current batch')
-                sys.stdout.write("done {}\n".format(count))
+                dump(data, idx)
+                idx += 1
                 data = []
+                sys.stdout.write("Done {}\n".format(count))
