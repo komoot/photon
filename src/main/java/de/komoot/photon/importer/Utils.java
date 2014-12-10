e9fda4b516c614821a7bd2935c1b1454783ca40b
Sander <sander.vantulden@falk.nl>
2014-12-10 10:27:29 +0100

    Added stripNonDigits util

--- src/main/java/de/komoot/photon/importer/Utils.java
+++ src/main/java/de/komoot/photon/importer/Utils.java
@@ -153,4 +153,18 @@ public class Utils {
 
 		return filteredNames;
 	}
+        
+        // http://stackoverflow.com/a/4031040/1437096
+        public static String stripNonDigits(
+                final CharSequence input /* inspired by seh's comment */){
+                final StringBuilder sb = new StringBuilder(
+                        input.length() /* also inspired by seh's comment */);
+                for(int i = 0; i < input.length(); i++){
+                        final char c = input.charAt(i);
+                        if(c > 47 && c < 58){
+                                sb.append(c);
+                        }
+                }
+                return sb.toString();
+        }
 }
