0eb9f537409ac141bf766b7f0d213f25d0928ace
Christoph Lingg <christoph@lingg.eu>
2014-12-12 18:27:33 +0100

    remove marvel plugin, it uses a lot of disk space for logging (relevant in production)

--- src/main/java/de/komoot/photon/importer/elasticsearch/Server.java
+++ src/main/java/de/komoot/photon/importer/elasticsearch/Server.java
@@ -79,7 +79,7 @@ public class Server {
 
 		if(!test) {
 			pluginManager = new PluginManager(new Environment(settings), null, PluginManager.OutputMode.VERBOSE, new TimeValue(30000));
-			for(String pluginName : new String[]{"mobz/elasticsearch-head", "polyfractal/elasticsearch-inquisitor", "elasticsearch/marvel/latest"}) {
+			for(String pluginName : new String[]{"mobz/elasticsearch-head", "polyfractal/elasticsearch-inquisitor"}) {
 				try {
 					pluginManager.downloadAndExtract(pluginName);
 				} catch(IOException e) {
