b71d88b69c8eb54d8f6aaf79a1d8fd2dc0057cc5
ec2-user <sdole@genvega.com>
2015-02-14 17:42:44 -0600

    decouple searcher from request handler photon request factory done

--- src/main/java/de/komoot/photon/PhotonQueryBuilder.java
+++ src/main/java/de/komoot/photon/PhotonQueryBuilder.java
@@ -2,13 +2,9 @@ package de.komoot.photon;
 
 import com.vividsolutions.jts.geom.Point;
 import org.elasticsearch.common.bytes.BytesReference;
-import org.elasticsearch.common.xcontent.ToXContent;
-import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.FilteredQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-
-import java.io.IOException;
 
 /**
  * Created by Sachin Dole on 2/12/2015.
@@ -20,23 +16,15 @@ public class PhotonQueryBuilder {
     private FilteredQueryBuilder filteredQueryBuilder;
 
 
-    private PhotonQueryBuilder(String query, Integer limit, Point locationForBias) throws IOException {
+    private PhotonQueryBuilder(String query, Integer limit, Point locationForBias) {
         this.query = query;
         this.limit = limit;
         this.locationForBias = locationForBias;
         filteredQueryBuilder = QueryBuilders.filteredQuery(QueryBuilders.boolQuery(), FilterBuilders.andFilter());
-        BytesReference bytes = filteredQueryBuilder.toXContent(JsonXContent.contentBuilder(), new ToXContent.MapParams(null)).bytes();
-        String s = new String(bytes.toBytes(), "UTF-8");
     }
 
     public static PhotonQueryBuilder builder(String query, Integer limit, Point locationForBias) {
-
-        try {
-            return new PhotonQueryBuilder(query, limit, locationForBias);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return new PhotonQueryBuilder(query, limit, locationForBias);
     }
 
 
