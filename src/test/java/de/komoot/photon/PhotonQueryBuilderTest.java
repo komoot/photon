3690d44d646545a31ef0137eb6b4144d95ab0661
sachin <sdole@genvega.com>
2015-02-17 12:10:16 -0600

    fixed the query part for collector

--- src/test/java/de/komoot/photon/PhotonQueryBuilderTest.java
+++ src/test/java/de/komoot/photon/PhotonQueryBuilderTest.java
@@ -7,6 +7,7 @@ import com.vividsolutions.jts.geom.PrecisionModel;
 import org.apache.commons.io.IOUtils;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
@@ -14,6 +15,7 @@ import org.elasticsearch.index.query.FilteredQueryBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -30,12 +32,14 @@ public class PhotonQueryBuilderTest {
         String createdJson = new String(bytes.toBytes(), "UTF-8");
         InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("json_queries/test_base_query.json");
         String expectedJsonString = IOUtils.toString(resourceAsStream);
-        JsonNode actualJson = new ObjectMapper().readTree(createdJson);
-        JsonNode expectedJson = new ObjectMapper().readTree(expectedJsonString);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
+        JsonNode actualJson = objectMapper.readTree(createdJson);
+        JsonNode expectedJson = objectMapper.readTree(expectedJsonString);
+        File outputFile = new File("C:\\Users\\sachind\\dev\\Projects\\photon\\src\\test\\resources\\json_queries\\output.json");
+        objectMapper.writeValue(outputFile, actualJson);
+
         Assert.assertEquals(expectedJson, actualJson);
-        System.out.println(actualJson.asToken());
-
-
     }
 
 }
