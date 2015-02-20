53067fe392fadbe80322e06181baf51550bb4bc7
ec2-user <sdole@genvega.com>
2015-02-20 14:36:12 -0600

    done with live tests against a test index. now to integrate it.

---
+++ src/main/java/de/komoot/photon/query/TagFilterQueryBuilder.java
@@ -0,0 +1,40 @@
+package de.komoot.photon.query;
+
+import com.vividsolutions.jts.geom.Point;
+import org.elasticsearch.index.query.QueryBuilder;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by sachi_000 on 2/20/2015.
+ */
+public interface TagFilterQueryBuilder {
+    TagFilterQueryBuilder withLimit(Integer limit);
+
+    TagFilterQueryBuilder withLocationBias(Point point);
+
+    TagFilterQueryBuilder withTags(Map<String, String> tags);
+
+    TagFilterQueryBuilder withKeys(Set<String> keys);
+
+    TagFilterQueryBuilder withValues(Set<String> values);
+
+    TagFilterQueryBuilder withoutTags(Map<String, String> tagsToExclude);
+
+    TagFilterQueryBuilder withoutKeys(Set<String> keysToExclude);
+
+    TagFilterQueryBuilder withoutValues(Set<String> valuesToExclude);
+    
+    TagFilterQueryBuilder withKeys(String... keys);
+
+    TagFilterQueryBuilder withValues(String... values);
+
+    TagFilterQueryBuilder withoutKeys(String... keysToExclude);
+
+    TagFilterQueryBuilder withoutValues(String... valuesToExclude);
+
+    QueryBuilder buildQuery();
+
+    Integer getLimit();
+}
