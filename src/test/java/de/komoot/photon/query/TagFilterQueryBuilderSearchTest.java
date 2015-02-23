fee1cbb89cd1fed72a4c5dc6927ed95ccb96d47a
ec2-user <sdole@genvega.com>
2015-02-22 22:03:33 -0600

    wrote a new test for find with key not value

--- src/test/java/de/komoot/photon/query/TagFilterQueryBuilderSearchTest.java
+++ src/test/java/de/komoot/photon/query/TagFilterQueryBuilderSearchTest.java
@@ -33,8 +33,8 @@ public class TagFilterQueryBuilderSearchTest extends ESBaseTester {
 
     private Client client;
     private List<PhotonDoc> testData;
-    
-    
+
+
     @Before
     public void setUp() throws IOException {
         setUpES();
@@ -60,6 +60,11 @@ public class TagFilterQueryBuilderSearchTest extends ESBaseTester {
         refresh();
     }
 
+    /**
+     * Find me all places named "berlin" that are tagged "tourism=attraction"
+     *
+     * @throws IOException
+     */
     @Test
     public void testFilterWithTagTourismAttraction() throws IOException {
         Client client = getClient();
@@ -70,6 +75,11 @@ public class TagFilterQueryBuilderSearchTest extends ESBaseTester {
         assertThat(searchResponse.getHits().getTotalHits(), is(2l));
     }
 
+    /**
+     * Find me all places named "berlin" that are tagged "tourism" with any value.
+     *
+     * @throws IOException
+     */
     @Test
     public void testKeyTourism() throws IOException {
         Client client = getClient();
@@ -79,6 +89,12 @@ public class TagFilterQueryBuilderSearchTest extends ESBaseTester {
         assertThat(searchResponse.getHits().getTotalHits(), is(8l));
     }
 
+    /**
+     * Find me all places named "berlin" that are tagged with the key "tourism" but not tagged with value "information". This method of including a key and excluding a value *
+     * separately may exclude another key that has the same value. See {@link TagFilterQueryBuilderSearchTest#testKeyTourismValueNotInformationAnotherWay()}
+     *
+     * @throws IOException
+     */
     @Test
     public void testKeyTourismValueNotInformation() throws IOException {
         Client client = getClient();
@@ -88,6 +104,28 @@ public class TagFilterQueryBuilderSearchTest extends ESBaseTester {
         assertThat(searchResponse.getHits().getTotalHits(), is(6l));
     }
 
+    /**
+     * Find me all places named "berlin" that are tagged with the key "tourism" but not tagged with value "information". This is similar to 
+     * {@link TagFilterQueryBuilderSearchTest#testKeyTourismValueNotInformation} but, does it explicitly for one key instead of excluding all values regardless of key. This method * does
+     * not exclude other keys that may have the same values.
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testKeyTourismValueNotInformationAnotherWay() throws IOException {
+        Client client = getClient();
+        Set<String> valueSet = ImmutableSet.of("information");
+        TagFilterQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin").withTagsNotValues(ImmutableMap.of("tourism", valueSet));
+        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
+        SearchResponse searchResponse = client.prepareSearch("photon").setSearchType(SearchType.QUERY_AND_FETCH).setQuery(queryBuilder).execute().actionGet();
+        assertThat(searchResponse.getHits().getTotalHits(), is(6l));
+    }
+
+    /**
+     * Find me all places named "berlin" that do not have the value "information" in their tags - no matter what key
+     *
+     * @throws IOException
+     */
     @Test
     public void testValueNotInformation() throws IOException {
         Client client = getClient();
@@ -97,6 +135,11 @@ public class TagFilterQueryBuilderSearchTest extends ESBaseTester {
         assertThat(searchResponse.getHits().getTotalHits(), is(12l));
     }
 
+    /**
+     * Find me all places named "berlin" that do not have the key "tourism" in their tags
+     *
+     * @throws IOException
+     */
     @Test
     public void testKeyNotTourism() throws IOException {
         Client client = getClient();
