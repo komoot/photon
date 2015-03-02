fa1ad300131532db272b2149accbd573ec5a644b
ec2-user <sdole@genvega.com>
2015-03-01 18:37:02 -0600

    only javadoc formatting.

--- src/main/java/de/komoot/photon/searcher/ElasticsearchSearcher.java
+++ src/main/java/de/komoot/photon/searcher/ElasticsearchSearcher.java
@@ -5,6 +5,7 @@ import org.elasticsearch.index.query.QueryBuilder;
 
 /**
  * This class handles all search execution that is specific to elastic search.
+ * <p/>
  * Created by Sachin Dole on 2/12/2015.
  */
 public interface ElasticsearchSearcher {
