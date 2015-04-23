152d5e0339900782466d6f26c72069a2713485a0
svantulden <sander.vantulden@falk.nl>
2015-04-23 11:21:23 +0200

    Defaults to refactored API

--- src/main/java/de/komoot/photon/App.java
+++ src/main/java/de/komoot/photon/App.java
@@ -133,13 +133,13 @@ public class App {
 		setIpAddress(args.getListenIp());
 
 		// setup search API
-		final Searcher searcher = new Searcher(esNodeClient);
-		get(new RequestHandler("api", searcher, args.getLanguages()));
-		get(new RequestHandler("api/", searcher, args.getLanguages()));
-                //get(new SearchRequestHandler("api", esNodeClient, args.getLanguages()));
-                //get(new SearchRequestHandler("api/", esNodeClient, args.getLanguages()));
-                //get(new ReverseSearchRequestHandler("reverse", esNodeClient, args.getLanguages()));
-                //get(new ReverseSearchRequestHandler("reverse/", esNodeClient, args.getLanguages()));
+		//final Searcher searcher = new Searcher(esNodeClient);
+		//get(new RequestHandler("api", searcher, args.getLanguages()));
+		//get(new RequestHandler("api/", searcher, args.getLanguages()));
+                get(new SearchRequestHandler("api", esNodeClient, args.getLanguages()));
+                get(new SearchRequestHandler("api/", esNodeClient, args.getLanguages()));
+                get(new ReverseSearchRequestHandler("reverse", esNodeClient, args.getLanguages()));
+                get(new ReverseSearchRequestHandler("reverse/", esNodeClient, args.getLanguages()));
                 
 		// setup update API
 		final NominatimUpdater nominatimUpdater = new NominatimUpdater(args.getHost(), args.getPort(), args.getDatabase(), args.getUser(), args.getPassword());
