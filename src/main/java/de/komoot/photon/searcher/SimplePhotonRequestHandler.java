5137284dfdf8d10f902055dd3efd7fb95b087597
Christoph Lingg <christoph@lingg.eu>
2015-08-11 16:28:24 +0200

    fix location bias for normal queries too

--- src/main/java/de/komoot/photon/searcher/SimplePhotonRequestHandler.java
+++ src/main/java/de/komoot/photon/searcher/SimplePhotonRequestHandler.java
@@ -1,5 +1,6 @@
 package de.komoot.photon.searcher;
 
+import com.vividsolutions.jts.geom.Point;
 import de.komoot.photon.query.PhotonQueryBuilder;
 import de.komoot.photon.query.PhotonRequest;
 import de.komoot.photon.query.TagFilterQueryBuilder;
@@ -8,12 +9,13 @@ import de.komoot.photon.query.TagFilterQueryBuilder;
  * Created by Sachin Dole on 2/12/2015.
  */
 public class SimplePhotonRequestHandler extends AbstractPhotonRequestHandler<PhotonRequest> implements PhotonRequestHandler<PhotonRequest> {
-    public SimplePhotonRequestHandler(ElasticsearchSearcher elasticsearchSearcher) {
-        super(elasticsearchSearcher);
-    }
+	public SimplePhotonRequestHandler(ElasticsearchSearcher elasticsearchSearcher) {
+		super(elasticsearchSearcher);
+	}
 
-    @Override
-    public TagFilterQueryBuilder buildQuery(PhotonRequest photonRequest) {
-        return PhotonQueryBuilder.builder(photonRequest.getQuery(), photonRequest.getLanguage());
-    }
+	@Override
+	public TagFilterQueryBuilder buildQuery(PhotonRequest photonRequest) {
+		Point point = photonRequest.getLocationForBias();
+		return PhotonQueryBuilder.builder(photonRequest.getQuery(), photonRequest.getLanguage()).withLocationBias(point);
+	}
 }
