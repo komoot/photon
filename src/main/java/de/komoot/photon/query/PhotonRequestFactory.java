2a0974de9df6c238ff920678382e15984467a30d
Peter <graphhopper@gmx.de>
2018-03-13 11:14:40 +0100

    remove distance_sort parameter from forward search

--- src/main/java/de/komoot/photon/query/PhotonRequestFactory.java
+++ src/main/java/de/komoot/photon/query/PhotonRequestFactory.java
@@ -20,7 +20,7 @@ public class PhotonRequestFactory {
     private final static GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
 
     protected static HashSet<String> m_hsRequestQueryParams = new HashSet<>(Arrays.asList("lang", "q", "lon", "lat",
-            "limit", "distance_sort", "osm_tag", "location_bias_scale"));
+            "limit", "osm_tag", "location_bias_scale"));
 
     public PhotonRequestFactory(Set<String> supportedLanguages) {
         this.languageChecker = new LanguageChecker(supportedLanguages);
