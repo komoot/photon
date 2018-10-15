ac876294dff883af18752d54b5b422d246eb4712
Holger Bruch <holger.bruch@systect.de>
2018-10-15 17:30:32 +0200

    Add osm_value to dedupe key (#367)
    Signed-off-by: Holger Bruch <holger.bruch@systect.de>

---
+++ src/test/java/de/komoot/photon/searcher/StreetDupesRemoverTest.java
@@ -0,0 +1,45 @@
+package de.komoot.photon.searcher;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.komoot.photon.Constants;
+
+public class StreetDupesRemoverTest {
+
+    @Test
+    public void testDeduplicatesStreets() {
+        StreetDupesRemover streetDupesRemover = new StreetDupesRemover("en");
+        List<JSONObject> allResults = new ArrayList<>();
+        allResults.add(createDummyResult("99999", "Main Street", "highway", "Unclassified"));
+        allResults.add(createDummyResult("99999", "Main Street", "highway", "Unclassified"));
+
+        List<JSONObject> dedupedResults = streetDupesRemover.execute(allResults);
+        Assert.assertEquals(1, dedupedResults.size());
+    }
+
+    @Test
+    public void testStreetAndBusStopNotDeduplicated() {
+        StreetDupesRemover streetDupesRemover = new StreetDupesRemover("en");
+        List<JSONObject> allResults = new ArrayList<>();
+        allResults.add(createDummyResult("99999", "Main Street", "highway", "bus_stop"));
+        allResults.add(createDummyResult("99999", "Main Street", "highway", "Unclassified"));
+
+        List<JSONObject> dedupedResults = streetDupesRemover.execute(allResults);
+        Assert.assertEquals(2, dedupedResults.size());
+    }
+    
+    private JSONObject createDummyResult(String postCode, String name, String osmKey,
+                    String osmValue) {
+        return new JSONObject().put(Constants.PROPERTIES, new JSONObject()
+                        .put(Constants.POSTCODE, postCode).put(Constants.NAME, name)
+                        .put(Constants.OSM_KEY, osmKey).put(Constants.OSM_VALUE, osmValue));
+    }
+
+}
