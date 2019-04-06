a1770883d98259fdc54aa923301a8504b90d9734
simonpoole <simon@poole.ch>
2019-04-06 21:15:58 +0200

    Add some API tests and some minor clean up of existing ones

---
+++ src/test/java/de/komoot/photon/ApiIntegrationTest.java
@@ -0,0 +1,159 @@
+package de.komoot.photon;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+import static spark.Spark.awaitInitialization;
+import static spark.Spark.awaitStop;
+import static spark.Spark.port;
+import static spark.Spark.stop;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+/**
+ * These test connect photon to an already running ES node (setup in ESBaseTester) so that we can directly test the API
+ * 
+ */
+public class ApiIntegrationTest extends ESBaseTester {
+    private static final int LISTEN_PORT = 30234;
+
+    /**
+     * Test that the Access-Control-Allow-Origin header is not set
+     *
+     */
+    @Test
+    public void testNoCors() {
+        try {
+            App.main(new String[] { "-cluster", clusterName, "-listen-port", Integer.toString(LISTEN_PORT), "-transport-addresses", "127.0.0.1" });
+            awaitInitialization();
+            HttpURLConnection connection = (HttpURLConnection) new URL("http://127.0.0.1:" + port() + "/api?q=berlin").openConnection();
+            String result = new BufferedReader(new InputStreamReader(connection.getInputStream())).lines().collect(Collectors.joining("\n"));
+            assertNull(connection.getHeaderField("Access-Control-Allow-Origin"));
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        } finally {
+            stop();
+            awaitStop();
+            deleteIndex();
+        }
+    }
+
+    /**
+     * Test that the Access-Control-Allow-Origin header is set to *
+     *
+     */
+    @Test
+    public void testCorsAny() {
+        try {
+            App.main(new String[] { "-cluster", "photon-test", "-listen-port", Integer.toString(LISTEN_PORT), "-transport-addresses", "127.0.0.1",
+                    "-cors-any" });
+            awaitInitialization();
+            HttpURLConnection connection = (HttpURLConnection) new URL("http://127.0.0.1:" + port() + "/api?q=berlin").openConnection();
+            String result = new BufferedReader(new InputStreamReader(connection.getInputStream())).lines().collect(Collectors.joining("\n"));
+            assertEquals("*", connection.getHeaderField("Access-Control-Allow-Origin"));
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        } finally {
+            stop();
+            awaitStop();
+            deleteIndex();
+        }
+    }
+
+    /**
+     * Test that the Access-Control-Allow-Origin header is set to a specific domain
+     *
+     */
+    @Test
+    public void testCorsOrigin() {
+        try {
+            App.main(new String[] { "-cluster", "photon-test", "-listen-port", Integer.toString(LISTEN_PORT), "-transport-addresses", "127.0.0.1",
+                    "-cors-origin", "www.poole.ch" });
+            awaitInitialization();
+            HttpURLConnection connection = (HttpURLConnection) new URL("http://127.0.0.1:" + port() + "/api?q=berlin").openConnection();
+            String result = new BufferedReader(new InputStreamReader(connection.getInputStream())).lines().collect(Collectors.joining("\n"));
+            assertEquals("www.poole.ch", connection.getHeaderField("Access-Control-Allow-Origin"));
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        } finally {
+            stop();
+            awaitStop();
+            deleteIndex();
+        }
+    }
+
+    /**
+     * Search for Berlin
+     *
+     */
+    @Test
+    public void testApi()  {
+        try {
+            App.main(new String[] { "-cluster", "photon-test", "-listen-port", Integer.toString(LISTEN_PORT), "-transport-addresses", "127.0.0.1" });
+            awaitInitialization();
+            HttpURLConnection connection = (HttpURLConnection) new URL("http://127.0.0.1:" + port() + "/api?q=berlin&limit=1").openConnection();
+            try {
+                JSONObject json = new JSONObject(
+                        new BufferedReader(new InputStreamReader(connection.getInputStream())).lines().collect(Collectors.joining("\n")));
+                JSONArray features = json.getJSONArray("features");
+                assertEquals(1, features.length());
+                JSONObject feature = features.getJSONObject(0);
+                JSONObject properties = feature.getJSONObject("properties");
+                assertEquals("way", properties.getString("osm_type"));
+                assertEquals("amenity", properties.getString("osm_key"));
+                assertEquals("information", properties.getString("osm_value"));
+                assertEquals("berlin", properties.getString("name"));
+            } catch (JSONException jsex) {
+                fail(jsex.getMessage());
+            }
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        } finally {
+            stop();
+            awaitStop();
+            deleteIndex();
+        }
+    }
+
+    /**
+     * Reverse geocode test
+     *
+     */
+    @Test
+    public void testApiReverse() {
+        try {
+            App.main(new String[] { "-cluster", "photon-test", "-listen-port", Integer.toString(LISTEN_PORT), "-transport-addresses", "127.0.0.1" });
+            awaitInitialization();
+            HttpURLConnection connection = (HttpURLConnection) new URL("http://127.0.0.1:" + port() + "/reverse/?lon=10&lat=47").openConnection();
+            try {
+                JSONObject json = new JSONObject(
+                        new BufferedReader(new InputStreamReader(connection.getInputStream())).lines().collect(Collectors.joining("\n")));
+                JSONArray features = json.getJSONArray("features");
+                assertEquals(1, features.length());
+                JSONObject feature = features.getJSONObject(0);
+                JSONObject properties = feature.getJSONObject("properties");
+                assertEquals("way", properties.getString("osm_type"));
+                assertEquals("amenity", properties.getString("osm_key"));
+                assertEquals("information", properties.getString("osm_value"));
+                assertEquals("berlin", properties.getString("name"));
+            } catch (JSONException jsex) {
+                fail(jsex.getMessage());
+            }
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        } finally {
+            stop();
+            awaitStop();
+            deleteIndex();
+        }
+    }
+}
--- src/test/java/de/komoot/photon/ESBaseTester.java
+++ src/test/java/de/komoot/photon/ESBaseTester.java
@@ -1,29 +1,72 @@
 package de.komoot.photon;
 
+import de.komoot.photon.elasticsearch.Importer;
 import de.komoot.photon.elasticsearch.Server;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
 import org.elasticsearch.client.Client;
 import org.junit.After;
+import org.junit.Before;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.PrecisionModel;
 
 import java.io.File;
 import java.io.IOException;
 
 /**
+ * Start an ES server with some test data that then can be queried in tests that extend this class
+ * 
  * @author Peter Karich
  */
 @Slf4j
 public class ESBaseTester {
+
+    public final String  clusterName = "photon-test";
+    private final String indexName   = "photon";
+
     private Server server;
 
+    GeometryFactory FACTORY = new GeometryFactory(new PrecisionModel(), 4326);
+
+    protected Client client;
+
+    private PhotonDoc createDoc(int id, String key, String value) {
+        ImmutableMap<String, String> nameMap = ImmutableMap.of("name", "berlin");
+        Point location = FACTORY.createPoint(new Coordinate(10., 47.));
+        return new PhotonDoc(id, "way", id, key, value, nameMap, null, null, null, 0, 0.5, null, location, 0, 0);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        setUpES();
+        ImmutableList<String> tags = ImmutableList.of("tourism", "attraction", "tourism", "hotel", "tourism", "museum", "tourism", "information", "amenity",
+                "parking", "amenity", "restaurant", "amenity", "information", "food", "information", "railway", "station");
+        client = getClient();
+        Importer instance = new Importer(client, "en");
+        for (int i = 0; i < tags.size(); i++) {
+            String key = tags.get(i);
+            String value = tags.get(++i);
+            PhotonDoc doc = this.createDoc(i, key, value);
+            instance.add(doc);
+            doc = this.createDoc(i + 1, key, value);
+            instance.add(doc);
+        }
+        instance.finish();
+        refresh();
+    }
+
     @After
-    public void tearDownClass() {
+    public void tearDown() {
         shutdownES();
     }
 
     public void setUpES() throws IOException {
-        server = new Server("photon-test", new File("./target/es_photon").getAbsolutePath(), "en", "").
-                start();
+        server = new Server(clusterName, new File("./target/es_photon_test").getAbsolutePath(), "en", "").start();
         server.recreateIndex();
         refresh();
     }
@@ -36,15 +79,17 @@ public class ESBaseTester {
         return server.getClient();
     }
 
-    private final String indexName = "photon";
-
     protected void refresh() {
         getClient().admin().indices().refresh(new RefreshRequest(indexName)).actionGet();
     }
 
+    /**
+     * Shutdown the ES node
+     */
     public void shutdownES() {
-        if (server != null)
+        if (server != null) {
             server.shutdown();
+        }
     }
 
     public void deleteIndex() {
