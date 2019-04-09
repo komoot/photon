d9c5ca032e79d1375955109b89f39112d96135b4
simonpoole <simon@poole.ch>
2019-04-09 14:05:46 +0200

    Externalize curated cities list
    This moves the list of curated cities list to a file and adds a small
    test that reading it actually works.
    
    Further it adds the boundary relations for
    
    -Algiers
    -Moscow
    -Vienna
    -Havanna
    
    to the list.

--- src/main/java/de/komoot/photon/nominatim/model/AddressRow.java
+++ src/main/java/de/komoot/photon/nominatim/model/AddressRow.java
@@ -1,12 +1,18 @@
 package de.komoot.photon.nominatim.model;
 
-import com.google.common.base.Objects;
-import lombok.Data;
-
-import java.util.Arrays;
-import java.util.Map;
-
 import static de.komoot.photon.Constants.STATE;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import com.google.common.base.Objects;
+
+import lombok.Data;
 
 /**
  * representation of an address as returned by nominatim's get_addressdata PL/pgSQL function
@@ -15,6 +21,10 @@ import static de.komoot.photon.Constants.STATE;
  */
 @Data
 public class AddressRow {
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(AddressRow.class);
+    
+    private static final String CURATED_CITIES = "curated_cities.txt";
+    
     final private long placeId;
     final private Map<String, String> name;
     final private String osmKey;
@@ -34,9 +44,30 @@ public class AddressRow {
     static private final long[] CURATED_CITY_RELATION_IDS;
 
     static {
-        long[] ids = new long[]{62772l, 62654l, 62532l, 62407l, 62340l, 62525l, 62640l, 62646l, 62644l, 62508l, 62634l, 62470l, 62531l, 62658l, 62594l, 62717l, 62430l, 62581l, 62414l, 62526l, 1829065l, 191645l, 62456l, 62539l, 62370l, 62562l, 62745l, 62403l, 62713l, 62675l, 62573l, 62523l, 62400l, 62768l, 62374l, 62522l, 62671l, 1800297l, 62638l, 62499l, 285864l, 62751l, 62396l, 62589l, 62381l, 62693l, 62652l, 62518l, 62598l, 62349l, 62701l, 27021l, 62512l, 62578l, 62748l, 62391l, 62484l, 62649l, 62449l, 27027l, 62347l, 62481l, 62630l, 62691l, 62590l, 62410l, 62385l, 62428l, 62591l, 62528l, 62724l, 62780l, 62734l, 62695l, 62409l, 62631l, 62629l, 62471l, 62642l, 62369l, 62411l, 62455l, 2168233l, 62405l, 62659l, 62720l, 62534l, 62685l, 62699l, 62352l, 62636l, 2793104l, 62450l, 172679l, 62495l, 62554l, 62493l, 62496l, 62444l, 62418l, 62453l, 62478l, 62464l, 62719l, 62422l, 62559l, 62782l};
-        Arrays.sort(ids);
-        CURATED_CITY_RELATION_IDS = ids;
+        final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        List<Long> idList = new ArrayList<>();
+        try (InputStream is = loader.getResourceAsStream(CURATED_CITIES); Scanner scanner = new Scanner(is);) {
+            if (is != null) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine().trim();
+                    if (!line.startsWith("#")) {
+                        try {
+                            idList.add(Long.parseLong(line));
+                        } catch (NumberFormatException nfe) {
+                            LOGGER.error(String.format("Unable to parse %s %s",line, nfe.getMessage()));
+                        }
+                    }
+                }
+            }
+        } catch (IOException ioe) {
+            LOGGER.error(String.format("Unable to read %s %s", CURATED_CITIES, ioe.getMessage()));
+        }
+        CURATED_CITY_RELATION_IDS = new long[idList.size()];
+        for (int i = 0; i < idList.size(); i++) {
+            CURATED_CITY_RELATION_IDS[i] = idList.get(i);
+        }
+        Arrays.sort(CURATED_CITY_RELATION_IDS);
+        LOGGER.info(String.format("Read %d entries from %s", CURATED_CITY_RELATION_IDS.length, CURATED_CITIES));
     }
 
     public boolean isStreet() {
@@ -62,10 +93,10 @@ public class AddressRow {
     /**
      * whether address row was manually marked as city
      *
-     * @return
+     * @return true if this row describes a city
      */
     public boolean isCuratedCity() {
-        if ("R".equals(osmType) && Arrays.binarySearch(CURATED_CITY_RELATION_IDS, osmId) >= 0) {
+        if ("R".equals(osmType) && Arrays.binarySearch(CURATED_CITY_RELATION_IDS, osmId) >= 0 ) {
             return true;
         }
         return false;
