b283d86eb48e8832f4b89f1acf3fc684fd3b7a71
Kate Belisle <kate.belisle@enabledata.com>
2021-01-06 07:31:39 -0500

    JarHell() hack for testing

---
+++ src/test/java/org/elasticsearch/bootstrap/JarHell.java
@@ -0,0 +1,16 @@
+package org.elasticsearch.bootstrap;
+
+import java.net.URL;
+import java.util.Set;
+import com.google.common.collect.ImmutableSet;
+
+public class JarHell {
+    private JarHell() {}
+    public static void checkJarHell() throws Exception {}
+//    public static void checkJarHell(URL urls[]) throws Exception {}
+    public static void checkJarHell(Set<URL> urls) throws Exception {}
+    public static void checkVersionFormat(String targetVersion) {}
+    public static void checkJavaVersion(String resource, String targetVersion) {}
+//  5.6 change  public static URL[] parseClassPath() {return new URL[]{};}
+    public static Set<URL> parseClassPath() {return ImmutableSet.of();}
+}
