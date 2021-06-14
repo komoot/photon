f02f676a6037b3b5e5c4d1a914e4d73485e57484
Sarah Hoffmann <lonvia@denofr.de>
2021-06-14 14:45:06 +0200

    cleanup type filter tests
    Factors out common code and reformats the tests to make them more
    readable.

--- src/test/java/de/komoot/photon/query/PhotonQueryBuilderSearchTest.java
+++ src/test/java/de/komoot/photon/query/PhotonQueryBuilderSearchTest.java
@@ -1,41 +1,34 @@
 package de.komoot.photon.query;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import de.komoot.photon.ESBaseTester;
 import de.komoot.photon.PhotonDoc;
 import de.komoot.photon.elasticsearch.Importer;
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Set;
+import java.util.Collections;
 
+import de.komoot.photon.elasticsearch.PhotonIndex;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
 
-/**
- * Created by Sachin Dole on 2/20/2015.
- */
 public class PhotonQueryBuilderSearchTest extends ESBaseTester {
+    private static final String[] TAGS = new String[]{"tourism", "attraction", "tourism", "hotel", "tourism", "museum", "tourism", "information", "amenity",
+            "parking", "amenity", "restaurant", "amenity", "information", "food", "information", "railway", "station"};
 
     @Before
     public void setUp() throws Exception {
         setUpES();
-        ImmutableList<String> tags = ImmutableList.of("tourism", "attraction", "tourism", "hotel", "tourism", "museum", "tourism", "information", "amenity",
-                "parking", "amenity", "restaurant", "amenity", "information", "food", "information", "railway", "station");
         Importer instance = makeImporter();
         double lon = 13.38886;
         double lat = 52.51704;
-        for (int i = 0; i < tags.size(); i++) {
-            String key = tags.get(i);
-            String value = tags.get(++i);
+        for (int i = 0; i < TAGS.length; i++) {
+            String key = TAGS[i];
+            String value = TAGS[++i];
             PhotonDoc doc = this.createDoc(lon, lat, i, i, key, value);
             instance.add(doc);
             lon += 0.00004;
@@ -49,19 +42,25 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
         refresh();
     }
 
+    private PhotonQueryBuilder baseQueryBerlin() {
+        return PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true);
+    }
+
+    private SearchResponse search(QueryBuilder queryBuilder) {
+        return getClient().prepareSearch(PhotonIndex.NAME).setSearchType(SearchType.QUERY_THEN_FETCH).setQuery(queryBuilder).execute().actionGet();
+    }
 
     /**
      * Find me all places named "berlin" that are tagged "tourism=attraction"
-     *
-     * @throws IOException
      */
     @Test
-    public void testFilterWithTagTourismAttraction() throws IOException {
-        Client client = getClient();
-        Set<String> valueSet = ImmutableSet.of("attraction");
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withTags(ImmutableMap.of("tourism", valueSet));
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testFilterWithTagTourismAttraction() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withTags(Collections.singletonMap("tourism", Collections.singleton("attraction")))
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(2l, searchResponse.getHits().getTotalHits());
     }
 
@@ -69,11 +68,13 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * Find me all places named "berlin" that are tagged with a value of "attraction".
      */
     @Test
-    public void testValueAttraction() throws IOException {
-        Client client = getClient();
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withValues("attraction");
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testValueAttraction() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withValues("attraction")
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(2l, searchResponse.getHits().getTotalHits());
     }
 
@@ -81,11 +82,13 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * Find me all places named "berlin" that are tagged with key "tourism".
      */
     @Test
-    public void testKeyTourism() throws IOException {
-        Client client = getClient();
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withKeys("tourism");
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testKeyTourism() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withKeys("tourism")
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(8l, searchResponse.getHits().getTotalHits());
     }
 
@@ -93,36 +96,41 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * Find me all places named "berlin" that are NOT tagged "tourism=attraction"
      */
     @Test
-    public void testFilterWithoutTagTourismAttraction() throws IOException {
-        Client client = getClient();
-        Set<String> valueSet = ImmutableSet.of("attraction");
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withoutTags(ImmutableMap.of("tourism", valueSet));
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testFilterWithoutTagTourismAttraction() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withoutTags(Collections.singletonMap("tourism", Collections.singleton("attraction")))
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(16l, searchResponse.getHits().getTotalHits());
     }
 
     /**
-     * Find me all places named "berlin" that do not have the value "information" in their tags - no matter what key
+     * Find me all places named "berlin" that do not have the value "information" in their TAGS - no matter what key
      */
     @Test
-    public void testValueNotInformation() throws IOException {
-        Client client = getClient();
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withoutValues("information");
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testValueNotInformation() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withoutValues("information")
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(12l, searchResponse.getHits().getTotalHits());
     }
 
     /**
-     * Find me all places named "berlin" that do not have the key "tourism" in their tags
+     * Find me all places named "berlin" that do not have the key "tourism" in their TAGS
      */
     @Test
-    public void testKeyNotTourism() throws IOException {
-        Client client = getClient();
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withoutKeys("tourism");
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testKeyNotTourism() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withoutKeys("tourism")
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(10l, searchResponse.getHits().getTotalHits());
     }
 
@@ -133,11 +141,14 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * {@link PhotonQueryBuilderSearchTest#testKeyTourismButValueNotInformation()}
      */
     @Test
-    public void testKeyTourismAndValueNotInformation() throws IOException {
-        Client client = getClient();
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withKeys("tourism").withoutValues("information");
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testKeyTourismAndValueNotInformation() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withKeys("tourism")
+                .withoutValues("information")
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(6l, searchResponse.getHits().getTotalHits());
     }
 
@@ -148,12 +159,13 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * {@link PhotonQueryBuilderSearchTest#testKeyTourismAndValueNotInformation}.
      */
     @Test
-    public void testKeyTourismButValueNotInformation() throws IOException {
-        Client client = getClient();
-        Set<String> valueSet = ImmutableSet.of("information");
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withTagsNotValues(ImmutableMap.of("tourism", valueSet));
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testKeyTourismButValueNotInformation() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withTagsNotValues(Collections.singletonMap("tourism", Collections.singleton("information")))
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(6l, searchResponse.getHits().getTotalHits());
     }
 
@@ -161,11 +173,13 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * Find me all places named "berlin" that are tagged without the keys "tourism" and "amenity".
      */
     @Test
-    public void testKeyNotTourismAndKeyNotAmenity() throws IOException {
-        Client client = getClient();
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withoutKeys("tourism", "amenity");
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testKeyNotTourismAndKeyNotAmenity() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withoutKeys("tourism", "amenity")
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(4l, searchResponse.getHits().getTotalHits());
     }
 
@@ -175,11 +189,14 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * on "amenity"
      */
     @Test
-    public void testKeyTourismAndKeyNotAmenity() throws IOException {
-        Client client = getClient();
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withKeys("tourism").withoutKeys("amenity");
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testKeyTourismAndKeyNotAmenity() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withKeys("tourism")
+                .withoutKeys("amenity")
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(8l, searchResponse.getHits().getTotalHits());
     }
 
@@ -187,11 +204,14 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * Find me all places named "berlin" that have value "information" but not key "amenity"
      */
     @Test
-    public void testValueInformationButKeyNotAmenity() throws IOException {
-        Client client = getClient();
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withValues("information").withoutKeys("amenity");
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
+    public void testValueInformationButKeyNotAmenity() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withValues("information")
+                .withoutKeys("amenity")
+                .buildQuery();
+
+        SearchResponse searchResponse = search(queryBuilder);
+
         assertEquals(4l, searchResponse.getHits().getTotalHits());
     }
 
@@ -199,16 +219,13 @@ public class PhotonQueryBuilderSearchTest extends ESBaseTester {
      * Find me all places named "berlin" that do not have the tag tourism=attraction
      */
     @Test
-    public void testTagNotTourismAttraction() throws IOException {
-        Client client = getClient();
-        Set<String> attraction = ImmutableSet.of("attraction");
-        PhotonQueryBuilder tagFilterQueryBuilder = PhotonQueryBuilder.builder("berlin", "en", Arrays.asList("en"), true).withoutTags(ImmutableMap.of("tourism", attraction));
-        QueryBuilder queryBuilder = tagFilterQueryBuilder.buildQuery();
-        SearchResponse searchResponse = search(client, queryBuilder);
-        assertEquals(16l, searchResponse.getHits().getTotalHits());
-    }
+    public void testTagNotTourismAttraction() {
+        QueryBuilder queryBuilder = baseQueryBerlin()
+                .withoutTags(Collections.singletonMap("tourism", Collections.singleton("attraction")))
+                .buildQuery();
 
-    private SearchResponse search(Client client, QueryBuilder queryBuilder) {
-        return client.prepareSearch("photon").setSearchType(SearchType.QUERY_AND_FETCH).setQuery(queryBuilder).execute().actionGet();
+        SearchResponse searchResponse = search(queryBuilder);
+
+        assertEquals(16l, searchResponse.getHits().getTotalHits());
     }
 }
