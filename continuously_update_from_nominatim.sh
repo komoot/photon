60a975414131fed584100f73b205722732886d93
Sarah Hoffmann <lonvia@denofr.de>
2021-08-11 16:31:46 +0200

    adapt update script for nominatim  CLI tool

--- continuously_update_from_nominatim.sh
+++ continuously_update_from_nominatim.sh
@@ -1,15 +1,28 @@
 #!/bin/bash -e
 
+# For Nominatim < 3.7 set this to the Nominatim build directory.
+# For newer versions, this must be the project directory of your import.
+NOMINATIM_DIR=
+
 while true
 do
     starttime=`date +%s`
 
-    cd $NOMINATIM_DIR
-    ./utils/update.php --no-npi --import-osmosis --no-index
+    # First consume updates in the Nominatim database.
+    # The important part here is to leave out the indexing step. This
+    # will be handled by Photon.
 
+    cd $NOMINATIM_DIR
+    # For Nominatim versions < 3.7 use the following:
+    # ./utils/update.php --import-osmosis --no-index
+    nominatim replication --once --no-index
+
+    # Now tell Photon to finish the updates and copy the new data into its
+    # own database.
     curl http://localhost:2322/nominatim-update
 
-    # sleep a bit if updates take less than a minute
+    # Sleep a bit if updates take less than a minute.
+    # If you consume hourly or daily diffs adapt the period accordingly.
     endtime=`date +%s`
     elapsed=$((endtime - starttime))
     if [[ $elapsed -lt 60 ]]
