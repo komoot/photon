5983d54dc273164a59d0704a3a7282ae1d4a2f9c
Hendrik Morée <hendrik@flitsmeister.nl>
2022-01-12 09:21:04 +0100

    Some tests to prove equality check

--- src/test/java/de/komoot/photon/PhotonDocTest.java
+++ src/test/java/de/komoot/photon/PhotonDocTest.java
@@ -40,6 +40,51 @@ public class PhotonDocTest {
         Assert.assertEquals("DE", doc.getCountryCode().getAlpha2());
     }
 
+    @Test
+    public void testSetNewAddressPart() {
+        PhotonDoc doc = simplePhotonDoc();
+
+        HashMap<String, String> streetNames = new HashMap<>();
+        streetNames.put("name", "parent place street");
+        Assert.assertTrue(doc.setAddressPartIfNew(AddressType.STREET, streetNames));
+    }
+
+    @Test
+    public void testSetOtherAddressPart() {
+        PhotonDoc doc = simplePhotonDoc();
+
+        HashMap<String, String> streetNames = new HashMap<>();
+        streetNames.put("name", "parent place street");
+        Assert.assertTrue(doc.setAddressPartIfNew(AddressType.STREET, streetNames));
+
+        HashMap<String, String> streetNames2 = new HashMap<>();
+        streetNames2.put("name", "parent place street 2");
+        Assert.assertFalse(doc.setAddressPartIfNew(AddressType.STREET, streetNames2));
+    }
+
+    @Test
+    public void testSetNewAddressPartTwice() {
+        PhotonDoc doc = simplePhotonDoc();
+
+        HashMap<String, String> streetNames = new HashMap<>();
+        streetNames.put("name", "parent place street");
+        Assert.assertTrue(doc.setAddressPartIfNew(AddressType.STREET, streetNames));
+        Assert.assertTrue(doc.setAddressPartIfNew(AddressType.STREET, streetNames));
+    }
+
+    @Test
+    public void testSetNewAddressPartTwiceWithOtherObject() {
+        PhotonDoc doc = simplePhotonDoc();
+
+        HashMap<String, String> streetNames1 = new HashMap<>();
+        streetNames1.put("name", "parent place street");
+        Assert.assertTrue(doc.setAddressPartIfNew(AddressType.STREET, streetNames1));
+
+        HashMap<String, String> streetNames2 = new HashMap<>();
+        streetNames2.put("name", "parent place street");
+        Assert.assertTrue(doc.setAddressPartIfNew(AddressType.STREET, streetNames2));
+    }
+
     private PhotonDoc simplePhotonDoc() {
         return new PhotonDoc(1, "W", 2, "highway", "residential").houseNumber("4");
     }
