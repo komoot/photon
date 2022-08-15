81571e761d71dd2a0657b785e44811ecbacec590
Julian Psotta <julianpsotta@gmail.com>
2022-08-15 13:32:54 +0200

    remove dump routine

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 # Inspired by https://github.com/thomasnordquist/photon-docker
 
-#
-# Extract elasticsearch index
-if [ ! -f "/photon/photon_data/photon-db-latest.tar.bz2" ]; then
-    echo "No suitable photon db found. Please check the data folder."
-    exit 1
-else
-    echo "Extract search index"
-    bzip2 -cd photon-db-latest.tar.bz2 | tar x -C photon_data/
-fi
 
 # Extract elasticsearch index
 if [ ! -d "/photon/photon_data/elasticsearch" ]; then
