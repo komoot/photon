8015daaa02b1590ce9b4896bef3a906e63d33571
Sarah Hoffmann <lonvia@denofr.de>
2025-10-03 16:28:47 +0200

    move common query code into BaseQueryBuilder

--- src/main/java/de/komoot/photon/opensearch/ReverseQueryBuilder.java
+++ src/main/java/de/komoot/photon/opensearch/ReverseQueryBuilder.java
@@ -1,71 +1,22 @@
 package de.komoot.photon.opensearch;
 
-import de.komoot.photon.searcher.TagFilter;
 import org.locationtech.jts.geom.Point;
-import org.opensearch.client.opensearch._types.FieldValue;
-import org.opensearch.client.opensearch._types.query_dsl.BoolQuery;
-import org.opensearch.client.opensearch._types.query_dsl.Query;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+public class ReverseQueryBuilder extends BaseQueryBuilder {
 
-public class ReverseQueryBuilder {
-    private final double radius;
-    private final Point location;
-    private final String queryStringFilter;
-    private final Set<String> layerFilter;
-
-    private final OsmTagFilter osmTagFilter = new OsmTagFilter();
-
-    public ReverseQueryBuilder(Point location, double radius, String queryStringFilter, Set<String> layerFilter) {
-        this.radius = radius;
-        this.location = location;
-        this.queryStringFilter = queryStringFilter != null && queryStringFilter.trim().length() > 0 ? queryStringFilter.trim() : null;
-        this.layerFilter = layerFilter;
-    }
-
-   public Query buildQuery() {
-        return BoolQuery.of(q -> {
-            q.filter(fq -> fq
+    public ReverseQueryBuilder(Point location, double radius) {
+        outerQuery.filter(fq -> fq
                     .geoDistance(gd -> gd
                             .field("coordinate")
                             .location(l -> l.latlon(ll -> ll.lat(location.getY()).lon(location.getX())))
                             .distance(radius + "km")));
-
-            boolean hasQuery = false;
-
-            if (queryStringFilter != null) {
-                q.must(qst -> qst.queryString(qs -> qs.query(queryStringFilter)));
-                hasQuery = true;
-            }
-
-            if (!layerFilter.isEmpty()) {
-                q.must(ftq -> ftq.terms(tq -> {
-                    List<FieldValue> terms = new ArrayList<>();
-                    for (var filter : layerFilter) {
-                        terms.add(FieldValue.of(filter));
-                    }
-                    return tq.field("type").terms(tt -> tt.value(terms));
-                }));
-                hasQuery = true;
-            }
-
-            if (!hasQuery) {
-                q.must(mq -> mq.matchAll(ma -> ma));
-            }
-
-            final var tagFilters = osmTagFilter.build();
-            if (tagFilters != null) {
-                q.filter(tagFilters);
-            }
-
-            return q;
-        }).toQuery();
     }
 
-    public ReverseQueryBuilder withOsmTagFilters(List<TagFilter> filters) {
-        osmTagFilter.withOsmTagFilters(filters);
-        return this;
+    public void addQueryFilter(String query) {
+        if (query != null && !query.isEmpty()) {
+            outerQuery.must(qst -> qst.queryString(qs -> qs
+                    .query(query)
+            ));
+        }
     }
 }
