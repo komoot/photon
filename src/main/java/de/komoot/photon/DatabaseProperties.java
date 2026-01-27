b4b41bfb2953031a6c143ed21d0844f53c5a0b3b
Sarah Hoffmann <lonvia@denofr.de>
2026-01-27 15:22:44 +0100

    add null annotation to DatabaseProperties.java

--- src/main/java/de/komoot/photon/DatabaseProperties.java
+++ src/main/java/de/komoot/photon/DatabaseProperties.java
@@ -2,6 +2,8 @@ package de.komoot.photon;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 import java.util.*;
 
@@ -9,20 +11,22 @@ import static de.komoot.photon.Server.DATABASE_VERSION;
 
 /**
  * Class collecting database global properties.
- *
- * The server is responsible for making the data persistent in the Photon database.
+ * <p>
+ * This class is marshalled and unmarshalled into OS table properties using Jackson.
  */
+@NullMarked
 public class DatabaseProperties {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final String[] DEFAULT_LANGAUGES = new String[]{"en", "de", "fr", "it"};
 
     private String[] languages = DEFAULT_LANGAUGES;
-    private Date importDate;
+    @Nullable private Date importDate;
     private boolean supportStructuredQueries = true;
     private boolean supportGeometries = false;
     private boolean synonymsInstalled = false;
     private ConfigExtraTags extraTags = new ConfigExtraTags();
 
+    @SuppressWarnings("unused")
     public void setDatabaseVersion(String version) {
         if (!DATABASE_VERSION.equals(version)) {
             LOGGER.error("Database has incompatible version '{}'. Expected: {}",
@@ -31,6 +35,7 @@ public class DatabaseProperties {
         }
     }
 
+    @SuppressWarnings("unused")
     public String getDatabaseVersion() {
         return DATABASE_VERSION;
     }
@@ -39,7 +44,7 @@ public class DatabaseProperties {
      * Return the list of languages for which the database is configured.
      * If no list was set, then the default is returned.
      *
-     * @return List of supported languages.
+     * @return Array of supported languages.
      */
     public String[] getLanguages() {
         return languages;
@@ -52,14 +57,14 @@ public class DatabaseProperties {
      *
      * @return This object for function chaining.
      */
-    public DatabaseProperties setLanguages(String[] languages) {
-        this.languages = languages == null ? DEFAULT_LANGAUGES : languages;
+    public DatabaseProperties setLanguages(String @Nullable[] languages) {
+        this.languages = (languages == null) ? DEFAULT_LANGAUGES : languages;
         return this;
     }
 
     /**
      * Set language list to the intersection between the existing list and the given list.
-     *
+     * <p>
      * The final list will use the same order as the given list.
      *
      * @param languageList Comma-separated list of two-letter language codes.
@@ -88,41 +93,40 @@ public class DatabaseProperties {
         }
     }
 
+    @Nullable
     public Date getImportDate() {
         return this.importDate;
     }
 
-    public DatabaseProperties setImportDate(Date importDate) {
+    public void setImportDate(@Nullable Date importDate) {
         this.importDate = importDate;
-        return this;
     }
 
     public boolean getSupportGeometries() {
         return supportGeometries;
     }
 
-    public DatabaseProperties setSupportGeometries(boolean supportGeometries) {
+    public void setSupportGeometries(boolean supportGeometries) {
         this.supportGeometries = supportGeometries;
-        return this;
     }
 
     // needed for backwards compatibility
-    public DatabaseProperties setSupportStructuredQueries(boolean supportStructuredQueries) {
+    @SuppressWarnings("unused")
+    public void setSupportStructuredQueries(boolean supportStructuredQueries) {
         this.supportStructuredQueries = supportStructuredQueries;
-        return this;
     }
 
     public void setExtraTags(List<String> extraTags) {
         this.extraTags = new ConfigExtraTags(extraTags);
     }
 
+    @SuppressWarnings("unused")
     public List<String> getExtraTags() {
         return extraTags.asConfigParam();
     }
 
-    public DatabaseProperties setSynonymsInstalled(boolean synonymsInstalled) {
+    public void setSynonymsInstalled(boolean synonymsInstalled) {
         this.synonymsInstalled = synonymsInstalled;
-        return this;
     }
 
     public boolean getSynonymsInstalled() {
